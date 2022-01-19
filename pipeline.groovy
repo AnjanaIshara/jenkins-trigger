07bd2c96d6a00c29fac0a4b6df8a64b26c2a5399
Anjana Ishara <anjanaishara08@gmail.com>
2022-01-19 15:45:15 +0530

    back to normal

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,15 +1,21 @@
 def jenkins_agent = 'master'
-def readprop = readFile 'variable.properties'
 
 pipeline{
     agent { label "$jenkins_agent" }
 
+    environment {
+        readprop = readProperties file: 'variable.properties'
+        SCHEDULE = "$readprop.SCHEDULE"
+        TZ = "$readprop.TZ"
+    }
 
     stages{
         stage('Welcome to jenkins') {
             steps {
                 script {
-                    echo "Hello anjana How are you?"  
+                    echo "Hello anjana How are you?"
+                    echo "${SCHEDULE}"
+                    echo "${TZ}"
                 }
             }
         }
