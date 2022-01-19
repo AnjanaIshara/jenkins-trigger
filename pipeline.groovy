d03867e6e00789d8c115b771eb9e2494aabe3a38
Anjana Ishara <anjanaishara08@gmail.com>
2022-01-19 17:34:57 +0530

    inroducing node

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,10 +1,11 @@
 def jenkins_agent = 'master'
 def SCHEDULE = "59 16 * * 1-5"
 def TZ = "TZ = Asia/Colombo"
+node{
+    readprop = readProperties file: 'variable.properties'
+    String cron_string = readprop['SCHEDULE']
+}
 
-readprop = readProperties file: 'variable.properties'
-
-String cron_string = readprop['SCHEDULE']
 pipeline{
     agent { label "$jenkins_agent" }
 
