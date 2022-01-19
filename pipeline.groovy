af6d0a311c6aa049e962ec38a84a69ce82730ba8
Anjana Ishara <anjanaishara08@gmail.com>
2022-01-19 14:45:00 +0530

    echo correction

--- pipeline.groovy
+++ pipeline.groovy
@@ -20,7 +20,7 @@ pipeline{
             steps {
                 script {
                     echo "Hello anjana How are you?"
-                    echo $SCHEDULE
+                    echo "${SCHEDULE}"
                 }
             }
         }
